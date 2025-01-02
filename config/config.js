0ef4d1c7a8462b8a53cfd48c149bd51321e0026b
Salih Usmanov <salih.usmanovv@gmail.com>
2025-01-02 11:47:19 +0200

    Config changes for deployment

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+module.exports = {
+  "development": {
+    "username": process.env.USER,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql"
+  }
+};

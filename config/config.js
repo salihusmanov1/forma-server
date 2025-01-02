2268c120bbbee46fb6792a48f3f663318e71b8f2
Salih Usmanov <salih.usmanovv@gmail.com>
2025-01-03 00:01:35 +0200

    Add .sequelizerc to fix the bug

--- config/config.js
+++ config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": process.env.DB_USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
     "username": process.env.DB_USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "dialect": "mysql"
   },

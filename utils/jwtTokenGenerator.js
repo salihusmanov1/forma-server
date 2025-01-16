194033b849a45272fa57d85012d5c919e4e32a14
Salih Usmanov <salih.usmanovv@gmail.com>
2025-01-16 18:02:51 +0200

    Fix jwtTokenGenerator.js

--- utils/jwtTokenGenerator.js
+++ utils/jwtTokenGenerator.js
@@ -5,7 +5,7 @@ const generateAccessToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: 'none',
+    sameSite: process.env.NODE_ENV !== "development" ? 'none' : 'strict',
     maxAge: 3 * 24 * 60 * 60 * 1000
   })
 };
